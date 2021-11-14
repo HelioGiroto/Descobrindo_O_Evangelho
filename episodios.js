fc9a4b8b89f5d3dae2e9c2cb7004fde87cac2572
heliogiroto <heliogiroto76@gmail.com>
2021-11-14 19:08:18 -0300

    Alterações em episodios.js

--- episodios.js
+++ episodios.js
@@ -4,15 +4,15 @@ let episodios = [
         data: "",
         hora: "",
         arquivo: "Mt0.mp3",
-        titulo: "Introdução",
-        descricao: "As 3 visões judaicas sobre o Messias"
+        titulo: "O contexto deste Evangelho",
+        descricao: "Introdução ao ambiente da época e as 3 visões judaicas sobre o Messias"
     }, 
 
     {
         data: "",
         hora: "",
         arquivo: "Mt1.mp3",
-        titulo: "Cap.  1",
+        titulo: "Cap. 1",
         descricao: "Gentios na descendência do Messias e Seu nascimento"
     }, 
 
@@ -20,15 +20,15 @@ let episodios = [
         data: "",
         hora: "",
         arquivo: "Mt2.mp3",
-        titulo: "Cap.  2 - parte 1",
-        descricao: "Quem é e qual a missão do Messias"
+        titulo: "Cap. 2 - parte 1",
+        descricao: "Quem é e qual a missão do Messias explicado pelas profecias e pelos anjos"
     }, 
 
     {
         data: "",
         hora: "",
         arquivo: "Mt2b.mp3",
-        titulo: "Cap.  2 - parte 2",
+        titulo: "Cap. 2 - parte 2",
         descricao: "A importância de José e Maria na vida de Jesus. A oferta dos três magos"
     }, 
 
@@ -36,7 +36,7 @@ let episodios = [
         data: "",
         hora: "",
         arquivo: "Mt3.mp3",
-        titulo: "Cap.  3 - parte 1",
+        titulo: "Cap. 3 - parte 1",
         descricao: "A aparição de João Batista"
     }, 
 
@@ -44,7 +44,7 @@ let episodios = [
         data: "",
         hora: "",
         arquivo: "Mt3b.mp3",
-        titulo: "Cap.  3 - parte 2",
+        titulo: "Cap. 3 - parte 2",
         descricao: "A mensagem que ensina a como entrar no Reino de Deus"
     }, 
 
@@ -52,7 +52,7 @@ let episodios = [
         data: "",
         hora: "",
         arquivo: "Mt3c.mp3",
-        titulo: "Cap.  3 - parte 3",
+        titulo: "Cap. 3 - parte 3",
         descricao: "O batismo de Jesus e a Tri-Unidade de Deus"
     }, 
 
@@ -60,7 +60,7 @@ let episodios = [
         data: "",
         hora: "",
         arquivo: "Mt4.mp3",
-        titulo: "Cap.  4 - parte 1",
+        titulo: "Cap. 4 - parte 1",
         descricao: "As tentações de Jesus e a existência do diabo"
     }, 
 
